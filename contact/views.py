abbfc5c83117db8d3882ee44f26a63b670c5ddbf
BaptisteMathon <baptistemathon2004@gmail.com>
2023-11-30 17:11:02 +0100

    contact v1

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .models import Contact
 
-# Create your views here.
-class Contact(View):
-    def get(self, request):
-        return render(request, 'contact/contact.html')
+def contactview(request):
+    if request.method == 'POST':
+        firstName = request.POST.get('prenom')
+        lastName = request.POST.get('nom')
+        email = request.POST.get('mail')
+        content = request.POST.get('message')
+        # print(f"----------Data: {firstName}, {lastName}, {email}, {content}")
+        if firstName and lastName and email and content:
+            contacts = Contact(firstName=firstName, lastName=lastName, email=email, content=content)
+            contacts.save()
+            # print('-----------Succés')
+            return redirect('contact')
+    # print('---------------Pas succés')
+    return render(request, 'contact/contact.html')
