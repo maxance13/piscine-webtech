f6e0c927f61cd4c062543005f3a42ab0fd79493a
maxance13 <115090832+maxance13@users.noreply.github.com>
2023-11-30 17:16:49 +0100

    app messagerie fini

--- messagerie/admin.py
+++ messagerie/admin.py
@@ -3,7 +3,7 @@ from .models import Message
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-  list_display = ('sender', 'recipient', 'subject', 'timestamp', 'is_read')
+  list_display = ('id', 'sender', 'recipient', 'subject', 'timestamp', 'is_read')
   list_filter = ('sender', 'recipient', 'timestamp', 'is_read')
   search_fields = ('sender', 'recipient', 'subject', 'body')
   
