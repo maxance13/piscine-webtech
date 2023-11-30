c7f1c5a48b4a607f8f50343b4bdfc2dbc0d66c94
BaptisteMathon <103061783+BaptisteMathon@users.noreply.github.com>
2023-11-30 21:10:53 +0100

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Contact
 
-def contactview(request):
-    if request.method == 'POST':
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'contact/contact.html', {'message_sent': False})
+    def post(self, request):
         firstName = request.POST.get('prenom')
         lastName = request.POST.get('nom')
         email = request.POST.get('mail')
         content = request.POST.get('message')
-        # print(f"----------Data: {firstName}, {lastName}, {email}, {content}")
         if firstName and lastName and email and content:
             contacts = Contact(firstName=firstName, lastName=lastName, email=email, content=content)
             contacts.save()
-            # print('-----------Succés')
-            return redirect('contact')
-    # print('---------------Pas succés')
-    return render(request, 'contact/contact.html')
+            return render(request, 'contact/contact.html', {'message_sent': True})
+        return render(request, 'contact/contact.html', {'message_sent': False})
