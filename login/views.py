e57761158b636877578ca79787d3cfe9fcafc042
maxance13 <115090832+maxance13@users.noreply.github.com>
2023-11-30 15:13:17 +0100

    base et nav dans le projet complet

--- login/views.py
+++ login/views.py
@@ -36,4 +36,4 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('..') 
+    return redirect('..'),HttpResponse("Vous vous êtes déconnecté avec succès.") 
